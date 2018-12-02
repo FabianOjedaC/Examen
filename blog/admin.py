79e66584a9c656b2ae6cf96d357cb9adcd08fd3c
FabianOjedaC <fabianojedac@gmail.com>
2018-12-01 21:28:43 -0300

    Some models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Empleado
 # Register your models here.
+class AdminEmpleado(admin.ModelAdmin):
+    list_display = ["nombreEmpleado", "apellidoEmpleado"]
+    class Meta:
+        model = Empleado
+
+
+admin.site.register(Empleado, AdminEmpleado)
--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
+
+class Empleado(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
+    nombreEmpleado=models.CharField(max_length=30)
+    apellidoEmpleado=models.CharField(max_length=30)
+
+
+class Tienda(models.Model):
+    nombreTienda=models.CharField(max_length=30)
+    direccionTienda=models.CharField(max_length=30)
+    ciudadTienda=models.CharField(max_length=30)
+    comunaTienda=models.CharField(max_length=30)
+    correoTienda=models.EmailField(max_length=254)
